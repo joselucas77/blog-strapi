0383804ed4796c86a941c577924059dd44044230
José Lucas <joselucasa937@gmail.com>
2023-12-11 20:30:11 -0300

    update

--- src/api/post/controllers/post.ts
+++ src/api/post/controllers/post.ts
@@ -2,6 +2,11 @@
  * post controller
  */
 
-import { factories } from '@strapi/strapi'
+import { factories } from "@strapi/strapi";
 
-export default factories.createCoreController('api::post.post');
+export default factories.createCoreController("api::post.post", {
+  count(ctx) {
+    var { query } = ctx.request;
+    return strapi.query("api::post.post").count({ where: query });
+  },
+});

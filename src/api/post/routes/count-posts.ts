48e469a575d296230b8ac7bf890109cb7796e781
José Lucas <joselucasa937@gmail.com>
2023-12-11 20:35:54 -0300

    update

--- src/api/post/routes/count-posts.ts
+++ src/api/post/routes/count-posts.ts
@@ -3,8 +3,8 @@ module.exports = {
     {
       // Path defined with a URL parameter
       method: "GET",
-      path: "/products/count",
-      handler: "product.count",
+      path: "/posts/count",
+      handler: "post.count",
     },
   ],
 };
